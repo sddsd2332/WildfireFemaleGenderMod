dd969789b74825b725af4d65e5095f73aa30822b
WildfireRomeo <wildfiregendermod@gmail.com>
2024-11-17 22:36:59 -0500

    I really want to just completely redesign the WardrobeBrowserScreen eventually, but I've just made a few quality of life changes for the time being.
    - Added simple player list on the left of the WardrobeBrowserScreen to show who is using the mod (improvements needed, also needs testing, but might work for initial release)
    - Added "playing with contributor" notice when connected to a server.

--- src/main/java/com/wildfire/main/WildfireGender.java
+++ src/main/java/com/wildfire/main/WildfireGender.java
@@ -18,9 +18,7 @@
 
 package com.wildfire.main;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 import com.mojang.logging.LogUtils;
 import com.wildfire.main.cloud.CloudSync;
@@ -36,6 +34,19 @@ public class WildfireGender implements ModInitializer {
 	public static final String MODID = "wildfire_gender";
 	public static final Logger LOGGER = LogUtils.getLogger();
 	public static final Map<UUID, PlayerConfig> PLAYER_CACHE = new HashMap<>();
+
+	public static final UUID CREATOR_UUID = UUID.fromString("23b6feed-2dfe-4f2e-9429-863fd4adb946");
+	public static final List<UUID> CONTRIBUTOR_UUIDS = Arrays.asList(
+			UUID.fromString("70336328-0de7-430e-8cba-2779e2a05ab5"), //celeste
+			UUID.fromString("64e57307-72e5-4f43-be9c-181e8e35cc9b"), //pupnewfster
+			UUID.fromString("618a8390-51b1-43b2-a53a-ab72c1bbd8bd"), //Kichura
+			UUID.fromString("33feda66-c706-4725-8983-f62e5e6cbee7"), //BlueLight
+			UUID.fromString("ad8ee68c-0aa1-47f9-b29f-f92fa1ef66dc"), //Diademiemi
+			UUID.fromString("8fb5e95d-7f41-4b4c-b8c5-4f15ea3fa2c1"), //Arcti.cc
+			UUID.fromString("3f36f7e9-7459-43fe-87ce-4e8a5d47da80"), //IzzyBizzy45
+			UUID.fromString("525b0455-15e9-49b7-b61d-f291e8ee6c5b") //Powerless001
+			//UUID.fromString("23b6feed-2dfe-4f2e-9429-863fd4adb946") //WildfireFGM (I'm not a contributor, silly!)
+	);
 
 	@Override
 	public void onInitialize() {
